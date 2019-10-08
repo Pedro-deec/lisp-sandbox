906a0d5536c7ef07cb9d771550857cbf96b9e6ce
Pedro-deec <pedrodeec@gmail.com>
2019-10-09 00:11:24 +0100

    Updated regex examples

--- Regex.lisp
+++ Regex.lisp
@@ -1,25 +1,32 @@
 ;;; Regular expressions
 
 (match-re "([A-Z])" "Amanha")
-T
-"A"
-"A"
+;; T
+;; "A"
+;; "A"
 
 (match-re "\\d" "1")
-T
-"1"
-
+;; T
+;; "1"
 
 (match-re "(H..).(o..)" "Hello World")
 T
-"Hello W"
-"Hel"
-"o W"
+;; "Hello W"
+;; "Hel"
+;; "o W"
 
 (match-re "l+" "Hello World")
-T
-"ll"
+;; T
+;; "ll"
 
 (match-re "Foo" "FOO" :case-fold t)
-T
-"FOO"
+;; T
+;; "FOO"
+
+(match-re "(.*)(-v)(.*)" "crens-ns-v5-3-0")
+;; T
+;; "crens-ns-v5-3-0"
+;; "crens-ns"
+;; "-v"
+;; "5-3-0"
+
